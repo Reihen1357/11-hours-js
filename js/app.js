4152ef472f9eca31e20e4a6f313a05f4173b898f
Reihen1357 <105004863+Reihen1357@users.noreply.github.com>
2022-05-19 20:51:22 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,24 +13,71 @@
 
 // Функции
 
-const personObj = {
-    name: 'Andy',
-    age: 21,
-    info: {
-        isMarried: true,
-        language: ['ru', 'en']
+// const personObj = {
+//     name: 'Andy',
+//     age: 21,
+//     info: {
+//         isMarried: true,
+//         language: ['ru', 'en']
+//     }
+// }
+
+
+// function persObjChange(person) {
+//     const copyPerson = JSON.parse(JSON.stringify(person)) //Создание копии объекта и его изменение (3 hours 5 min video)
+//     copyPerson.name = 'Carl'
+//     copyPerson.info.isMarried = false
+//     copyPerson.info.language[1] = 'de'
+//     return copyPerson
+// }
+
+// const copyPerson = persObjChange(personObj)
+// console.log(personObj)
+// console.log(copyPerson)
+
+/* Колбэк ф-ия
+    function printName() {
+//     console.log('Andrey')
+// }
+
+// function print(smFunc) {
+//     smFunc()
+// }
+    print(printName)
+
+    //---------------------------------
+    function printMyName() {
+        console.log('Andrey)
     }
+
+    setTimeout(printMyName, 2000); */
+
+ // Области видимости
+
+// const a = 5
+
+//  function myFn() {
+//      function innerFn() {
+//          console.log(a)
+//      }
+//    return innerFn()
+//  }
+
+//  myFn()
+
+let a
+let b
+
+function func() {
+    let b
+    a = true
+    b = 10
+    console.log(b)
 }
 
+func()
 
-function persObjChange(person) {
-    const copyPerson = JSON.parse(JSON.stringify(person)) //Создание копии объекта и его изменение (3 hours 5 min video)
-    copyPerson.name = 'Carl'
-    copyPerson.info.isMarried = false
-    copyPerson.info.language[1] = 'de'
-    return copyPerson
-}
+console.log(a)
+console.log(b)
 
-const copyPerson = persObjChange(personObj)
-console.log(personObj)
-console.log(copyPerson)
+//------------------------- (4 hours)
