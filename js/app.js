c03baab6c12c396d6e704eb77c240a773cc57940
Reihen1357 <105004863+Reihen1357@users.noreply.github.com>
2022-05-20 15:47:11 +0300

    Update app.js
    6 h, 20 min

--- js/app.js
+++ js/app.js
@@ -65,7 +65,7 @@
 
 //  myFn()
 
-let a
+/* let a
 let b
 
 function func() {
@@ -78,6 +78,134 @@ function func() {
 func()
 
 console.log(a)
-console.log(b)
+console.log(b) */
 
-//------------------------- (4 hours)
+//-------------------------
+// Объединение объектов с помощью оператора ... (оператор разделения объектва на св-ва)
+
+/* const buttonInfo = {
+    text: 'buy'
+}
+
+const buttonStyle = {
+    color: 'red',
+    width: 300,
+    hight: 200
+}
+
+const button = {
+    ...buttonStyle,
+    info: {
+        ...buttonInfo
+    }
+}
+
+console.log(button) */
+
+/* const myName = 'Andrey'
+const myCity = 'Vrn'
+
+console.log(`Hello, my name ${myName}, I'm living in ${myCity}`) //Конкатенация строк */
+
+//Function expression - функциональное выражение / Function declaration - объявленная ф-ия
+
+/*setTimeout(() => {
+    console.log('Message')
+}, 2000) */
+
+
+/* const funcArrow = (a, b) => {
+    let c = 10
+    c += a + b
+    return console.log(c)
+}
+
+funcArrow(3,4) */
+
+
+//Значение параметра ф-ии по умолчанию --------------------------
+
+/* function multByFactor(value, multiplier = 1) {
+    return value * multiplier
+}
+
+console.log(multByFactor(10, 3))
+console.log(multByFactor(4))
+
+const mult = function(value, mult = 1) {
+    return value * mult
+}
+
+console.log(mult(5, 3))
+console.log(mult(4))
+
+const multArrow = (value, mult = 1) => {
+    return value * mult
+}
+
+console.log(multArrow(7, 2))
+console.log(multArrow(2)) */
+
+
+
+/* const newPost = (post, addedAt = Date()) => {return({
+    ...post,
+    addedAt
+ })}
+    
+
+
+const firstPost = {
+    id: 1,
+    author: 'Andrey',
+    Message: {
+        text: 'Hi',
+        likes: 20
+    },
+    isMarried: false,
+    friends: 70
+}
+
+console.log(newPost(firstPost)) */
+
+
+/* const fnWithError = () => {
+    throw new Error('Some error')
+}
+try {
+    fnWithError()
+} catch (error) {
+    console.error(error)
+    console.log(error.message)
+}
+
+console.log('Continue...') */
+
+//-----------------------(Массивы)------------------------------
+
+//const newArr = [1, 2, '3', 4, {id:1, someName: 'Andy'}, true]
+
+/* newArr.push({age: 21, isMarried: false})
+
+console.log(newArr)
+
+const removedElem = newArr.pop()
+
+console.log(removedElem)
+
+console.log(newArr) */
+
+//const newArr = [1, 2, 3, 4, 5]
+
+/*const copyNewArr2 = newArr2.forEach(el => console.log(el * 2)) // Ничего не возвращает, не мутирует исходный массив
+
+console.log(newArr2)
+console.log(copyNewArr2) //undefined */
+
+/* const copyNewArr = newArr.map(el => el * 3)
+
+console.log(copyNewArr)
+console.log(newArr) */
+
+
+
